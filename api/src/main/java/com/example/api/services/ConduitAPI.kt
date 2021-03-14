84c0248575163d7f48d562d4ee2c8e3635d11e78
Shivam170 <shivamsharma4723@gmail.com>
2021-03-14 22:11:17 +0530

    Second Commmit

--- api/src/main/java/com/example/api/services/ConduitAPI.kt
+++ api/src/main/java/com/example/api/services/ConduitAPI.kt
@@ -4,8 +4,13 @@ import com.example.api.models.ArticlesResponse
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ConduitAPI {
     @GET("articles")
-    fun getArticles() : Call<ArticlesResponse>
+    suspend fun getArticles(
+            @Query("author") author : String? = null,
+            @Query("favourited") favourited : String? = null,
+            @Query("tag") tags: List<String>? = null
+    ) : Response<ArticlesResponse>
 }
--- api/src/test/java/com/example/conduit/ConduitClientTests.kt
+++ api/src/test/java/com/example/conduit/ConduitClientTests.kt
@@ -1,6 +1,7 @@
 package com.example.conduit
 
 import com.example.api.ConduitClient
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 
@@ -8,7 +9,27 @@ class ConduitClientTests {
     private var conduitClient = ConduitClient()
     @Test
     fun `GET articles`(){
-       val articles =  conduitClient.api.getArticles().execute()
-        assertNotNull(articles.body()?.articles)
+        runBlocking {
+            val articles =  conduitClient.api.getArticles()
+            assertNotNull(articles.body()?.articles)
+        }
+//       val articles =  conduitClient.api.getArticles().execute()
+//        assertNotNull(articles.body()?.articles)
+    }
+
+    @Test
+    fun `GET articles by author`(){
+        runBlocking {
+            val articles =  conduitClient.api.getArticles(author  = "444")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by tags`(){
+        runBlocking {
+            val articles =  conduitClient.api.getArticles(tags = listOf("dragons","butts"))
+            assertNotNull(articles.body()?.articles)
+        }
     }
 }
